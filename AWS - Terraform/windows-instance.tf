c645f3fb7f6e7553042bc54c39082629e2ae31c5
Fleorens <florian.deruwez@outlook.com>
2022-01-14 18:13:57 +0100

    Added automated installation ADDS/DNS

--- AWS - Terraform/windows-instance.tf
+++ AWS - Terraform/windows-instance.tf
@@ -2,8 +2,20 @@ resource "aws_key_pair" "mykey" {
   key_name   = "mykey"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
+
+resource "aws_instance" "win-example" {
+  ami           = var.WIN_AMIS[var.AWS_REGION]
+  instance_type = "t2.medium"
+  key_name      = aws_key_pair.mykey.key_name
+  user_data = data.template_file.userdata_win.rendered
+  vpc_security_group_ids=["${aws_security_group.allow-all.id}"]
+
+  tags = {
+    Name = "Windows_Server"
+  }
  
- 
+}
+
 data "template_file" "userdata_win" {
   template = <<EOF
 <script>
@@ -11,27 +23,41 @@ echo "" > _INIT_STARTED_
 net user ${var.INSTANCE_USERNAME} /add /y
 net user ${var.INSTANCE_USERNAME} ${var.INSTANCE_PASSWORD}
 net localgroup administrators ${var.INSTANCE_USERNAME} /add
-echo ${base64encode(file("./test.txt"))} > tmp2.b64 && certutil -decode tmp2.b64 C:/test.txt
+Enable-PSRemoting -Force
+winrm quickconfig -q & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"}
 echo "" > _INIT_COMPLETE_
 </script>
+<powershell>
+Install-WindowsFeature RSAT-AD-Tools -IncludeManagementTools -IncludeAllSubFeature
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
+Install-WindowsFeature DNS -IncludeManagementTools -IncludeAllSubFeature
+
+$SafeModeClearPassword = "passwd12345+"
+$SafeModeAdministratorPassword = ConvertTo-SecureString $SafeModeClearPassword -AsPlaintext -Force
+$DomainNameDNS = "groupefyb.fr"
+$DomainNameNetbios = "GROUPEFYB"
+
+$ForestConfiguration = @{
+'-DatabasePath'= 'C:\Windows\NTDS';
+'-DomainMode' = 'Default';
+'-DomainName' = $DomainNameDNS;
+'-DomainNetbiosName' = $DomainNameNetbios;
+'-SafeModeAdministratorPassword' = $SafeModeAdministratorPassword;
+'-ForestMode' = 'Default';
+'-InstallDns' = $true;
+'-LogPath' = 'C:\Windows\NTDS';
+'-NoRebootOnCompletion' = $false;
+'-SysvolPath' = 'C:\Windows\SYSVOL';
+'-Force' = $true;
+'-CreateDnsDelegation' = $false }
+
+Import-Module ADDSDeployment
+Install-ADDSForest @ForestConfiguration
+</powershell>
 <persist>false</persist>
 EOF
 }
- 
- 
-resource "aws_instance" "win-example" {
-  ami           = var.WIN_AMIS[var.AWS_REGION]
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.mykey.key_name
-  user_data = data.template_file.userdata_win.rendered
-  vpc_security_group_ids=["${aws_security_group.allow-all.id}"]
- 
-  tags = {
-    Name = "Windows_Server"
-  }
- 
-}
- 
+
 output "ip" {
  
 value="${aws_instance.win-example.public_ip}"
