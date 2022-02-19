baf65f673dc135b06d5eb38d1351b6adbb94ee55
Fleorens <florian.deruwez@outlook.com>
2022-02-19 18:25:54 +0100

    Add some change for Nextcloud (.env)

--- AWS - Terraform/linux-instance.tf
+++ AWS - Terraform/linux-instance.tf
@@ -4,9 +4,21 @@ resource "aws_instance" "cloud_srv" {
   subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
   key_name=aws_key_pair.mykey.key_name
   vpc_security_group_ids = ["${aws_security_group.allow-all.id}"]
+
   provisioner "file" {
     source      = "docker-compose.yml"
     destination = "/home/ec2-user/docker-compose.yml"
+    connection {
+    host = self.public_ip
+    type = "ssh"
+    user = "ec2-user"
+    private_key = file("mykey.pem")
+  }
+  }
+
+    provisioner "file" {
+    source      = ".env"
+    destination = "/home/ec2-user/.env"
     connection {
     host = self.public_ip
     type = "ssh"
@@ -21,7 +33,7 @@ resource "aws_instance" "cloud_srv" {
       "sudo amazon-linux-extras install docker -y",
       "sudo service docker start",
       "sudo usermod -a -G docker ec2-user",
-      "sudo service docker enable",
+      "sudo systemctl enable docker",
       "sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose",
